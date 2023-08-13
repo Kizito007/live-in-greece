df7fe26f80be404dea4ecb86674eb38d40a5a4ea
Yvonne Nwaka <nwakakelechi49@gmail.com>
2023-08-13 16:00:22 +0100

    initial commit

---
+++ app/components/Discover.js
@@ -0,0 +1,23 @@
+const Discover = () => {
+  return (
+    <div className="discoveries">
+      <h1>Discover Innovation Opportunities</h1>
+      <div className="opportunities-cover">
+        <div className="opportunities">
+          <p id="talents">Talents</p>
+          <button>&rarr;</button>
+        </div>
+        <div className="opportunities">
+          <p id="enterpreneurs">Enterpreneurs</p>
+          <button>&rarr;</button>
+        </div>
+        <div className="opportunities">
+          <p id="investors">Investors</p>
+          <button>&rarr;</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
